8a0719fd54000e867b9f4bf00aad0fce4de84e90
g-gekko <ggekko0708@gmail.com>
2017-03-17 14:07:18 +0400

    update controller

--- src/main/java/net/controllers/MainController.java
+++ src/main/java/net/controllers/MainController.java
@@ -1,7 +1,16 @@
 package net.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
 public class MainController {
+
+    @RequestMapping("/login")
+    public String login(Map<String, Object> model) {
+
+        return "login";
+    }
 }
