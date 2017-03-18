ea521e7e9ba90ffa2ac41f2c4f4ee4448b2751ce
sergey <sergey3440@gmail.com>
2017-03-19 00:30:40 +0400

    employees_list

--- src/main/java/net/controllers/RukController.java
+++ src/main/java/net/controllers/RukController.java
@@ -1,6 +1,8 @@
 package net.controllers;
 
+import net.dto.EmployeeDto;
 import net.dto.UserDto;
+import net.service.EmployeeService;
 import net.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,16 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class RukController {
 
     private final UserService userService;
+    private final EmployeeService employeeService;
 
     @Autowired
-    public RukController(UserService userService) {
+    public RukController(UserService userService, EmployeeService employeeService) {
         this.userService = userService;
+        this.employeeService = employeeService;
     }
 
     @RequestMapping(value = {"/reg_ruk"}, method = RequestMethod.GET)
@@ -56,6 +61,27 @@ public class RukController {
             model.put("ruk", userService.getUser(login));
         }
         return "redirect:edit_ruk";
+    }
+
+    @RequestMapping(value = {"/addEmployee"}, method = RequestMethod.POST)
+    public String addEmployees(Map<String, Object> model,
+                               @RequestParam(value = "name", required = true) String name,
+                               @RequestParam(value = "login", required = true) String login,
+                               @RequestParam(value = "password", required = true) String pass,
+                               @RequestParam(value = "email", required = true) String email,
+                               @RequestParam(value = "role", required = true) List<String> roles
+    ) {
+        userService.addCollaborator(login, pass, roles, email);
+        employeeService.addEmployee(login, name);
+        return "employees_list";
+    }
+
+    @RequestMapping("/employees_list")
+    public String employees_list(Map<String, Object> model) {
+        List<EmployeeDto> employees = employeeService.getAllEmployees();
+        model.put("employees", employees);
+
+        return "employees_list";
     }
 
 }
