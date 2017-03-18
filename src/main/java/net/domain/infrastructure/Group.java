831b36724a4c43bb32e48afa1ca908572f4f0cd4
sergey <sergey3440@gmail.com>
2017-03-18 20:54:11 +0400

    domain groups

---
+++ src/main/java/net/domain/infrastructure/Group.java
@@ -0,0 +1,30 @@
+package net.domain.infrastructure;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.domain.contacts.Contact;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Group {
+    @Id
+    private long id;
+    private String name;
+    private Сourses сourses;
+    @ManyToMany
+    private List<Lesson> lessons;
+    @ManyToMany
+    private List<Contact> contacts;//students
+    @ManyToMany
+    private List<Employee> employees;
+}
---
+++ src/main/java/net/domain/infrastructure/Paragraf.java
@@ -0,0 +1,21 @@
+package net.domain.infrastructure;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Paragraf {
+    @Id
+    private long id;
+    private String name;
+}
