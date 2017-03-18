35eb9b929aa14c2b5ba541142282d23d3577b99e
skyhook19 <bronfirstring@gmail.com>
2017-03-18 22:21:02 +0400

    update employee service and added teacher role

--- src/main/java/net/service/EmployeeService.java
+++ src/main/java/net/service/EmployeeService.java
@@ -1,15 +1,41 @@
 package net.service;
 
+import net.dao.DaoUserImpl;
+import net.dao.employee.DaoEmployee;
+import net.dao.users.DaoRole;
+import net.dao.users.DaoUser;
 import net.domain.employee.Employee;
+import net.domain.infrastructure.Account;
+import net.domain.users.User;
+import net.dto.EmployeeDto;
+import net.service.converters.ConverterEmployee;
+import net.service.converters.ConverterUsers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EmployeeService {
+    private static DaoEmployee daoEmployee;
+    private static ConverterEmployee converterEmployee;
 
+    @Autowired
+    public EmployeeService(DaoEmployee daoEmployee, ConverterEmployee converterEmployee) {
+        this.daoEmployee = daoEmployee;
+        this.converterEmployee = converterEmployee;
+    }
 
-//    public List<Employee> getAllEmployees(){
-//        return daoEmployeeImpl.getAllEmployees(getCurrentUser().getAccount());
-//    }
+    public List<EmployeeDto> getAllEmployees(){
+        Account account = getCurrentEmployee().getAccount();
+        List<Employee> employees = daoEmployee.findByAccount(account);
+        return converterEmployee.convertToEmployeeDto(employees);
+    }
+
+    private Employee getCurrentEmployee() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return daoEmployee.findOneByLogin(authentication.getName());
+    }
 }
