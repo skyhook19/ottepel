a768d1dff45a86ed17c4ddd6a27542ad08542bb5
sergey <sergey3440@gmail.com>
2017-03-18 16:30:07 +0400

    clean

--- ottepel/src/main/resources/static/template/js/script.js
+++
@@ -1,6 +0,0 @@
-//Генерим пароль
-function generatePass(id) {
-    var el = document.getElementById(id);
-    el.type = 'text';
-    el.value = Math.random().toString(36).slice(2, 2 + Math.max(1, Math.min(10, 10)));
-}
